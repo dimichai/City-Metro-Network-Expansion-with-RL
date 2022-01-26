1108ffdf76589f66194b7825f0f54b8322094e4d
dimichai <dimitris@hey.com>
2022-01-26 15:47:53 +0100

    constants.py working dir change

--- constants.py
+++ constants.py
@@ -1,3 +1,4 @@
 import os
 
-WORKING_DIR = os.path.join('C:/', 'Users', 'Dimitris', 'Documents', 'GitHub', 'City-Metro-Network-Expansion-with-RL-dimichai')
+# WORKING_DIR = os.path.join('C:/', 'Users', 'Dimitris', 'Documents', 'GitHub', 'City-Metro-Network-Expansion-with-RL-dimichai')
+WORKING_DIR = os.path.join('.')
